7ad22754ab5523f1a977855032b5c768365b8b0f
Zhang <p3.1415@qq.com>
2014-09-07 01:38:33 +0800

    Add listening

--- cloudio/server.hpp
+++ cloudio/server.hpp
@@ -11,17 +11,29 @@ namespace cloudio{
     public:
         typedef boost::signals2::signal<void (shared_ptr<socket>)>      connection_signal;
         typedef typename connection_signal::slot_type                   connection_slot;
+        
+        typedef boost::signals2::signal<void (void)>                    listening_signal;
+        typedef typename listening_signal::slot_type                    listening_slot;
 
         server(io_service &iosev)
         : _iosev(iosev)
-        , _sp_websocket_server(new websocket::server(iosev)){ }
+        , _sp_websocket_server(new websocket::server(iosev))
+        , _sp_listening_signal(new listening_signal){ }
 
         void listen(size_t port){
             _sp_websocket_server->listen(port);
+            auto self = shared_from_this();
+            _sp_websocket_server->on_listening([self](){
+                self->emit_listening();
+            });
         }
 
         void on_connection(connection_slot f){
             of("/")->on_connection(f);
+        }
+        
+        void on_listening(listening_slot f){
+            _sp_listening_signal->connect(f);
         }
 
         shared_ptr<channel> of(const std::string &path){
@@ -39,9 +51,17 @@ namespace cloudio{
                 return _sp_channels[path];
             }
         }
+        
+    private:
+        
+        void emit_listening(){
+            (*_sp_listening_signal)();
+        }
 
     private:
         io_service          &_iosev;
+        
+        shared_ptr<listening_signal>                    _sp_listening_signal;
 
         shared_ptr<websocket::server>                   _sp_websocket_server;
         std::map<std::string, shared_ptr<channel>>      _sp_channels;
